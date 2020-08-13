748371cfd32cc4b66459cbcd44da239d22a87e8f
Reena16 <Reenabhaira98@gmail.com>
2020-08-13 20:59:00 +0530

    model added

---
+++ HomeLoan_Gladiator/src/main/java/com/lti/model/UserInfo.java
@@ -0,0 +1,14 @@
+package com.lti.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class UserInfo {
+
+	private int userId;
+	private String userName;
+	
+	
+}
